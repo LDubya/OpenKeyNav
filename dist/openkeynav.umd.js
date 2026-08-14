fc6df9585b4e055879cf020e3168322daab6ad9c
Larry <Larryw09@gmail.com>
2026-08-14 11:25:42 -0400

    version ++

--- dist/openkeynav.umd.js
+++ dist/openkeynav.umd.js
@@ -41,7 +41,7 @@
 	  value: true
 	});
 	version.version = void 0;
-	version.version = "0.1.271";
+	version.version = "0.1.272";
 
 	var signals = {};
 
--- src/version.js
+++ src/version.js
@@ -1,1 +1,1 @@
-export const version = "0.1.271";
+export const version = "0.1.272";
